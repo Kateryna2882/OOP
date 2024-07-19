adedb6662623493239ee2e97632f0ead6a0fe985
Kateryna2882 <govoryhina82@gmail.com>
2024-07-19 18:47:48 +0300

    Add fields and constructor

---
+++ src/Employee.java
@@ -0,0 +1,15 @@
+public class Employee {
+    private String name;
+    private int age;
+    private String position;
+    private String email;
+    private String telephone;
+
+    public Employee(String name, int age, String position, String email, String telephone) {
+        this.name = name;
+        this.age = age;
+        this.position = position;
+        this.email = email;
+        this.telephone = telephone;
+    }
+}
